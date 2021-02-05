432f9c90f412adfff9334ee454481a646cde4c64
weijiew <836678589@qq.com>
2021-02-05 19:41:27 +0800

    :art: init

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+int poolsize;
+
+int main(int argc, char **argv) {
+    int i, fd;
+
+    argc--;
+    argv++;
+
+    poolsize = 256 * 1024;  //
+    line = 1;
+
+    if
+
+    return 0;
+}
