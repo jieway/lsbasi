f06e7c1f67e1d9c3dc34a7210d8d4c9025ca5b11
weijiew <836678589@qq.com>
2021-02-13 23:32:43 +0800

    :sparkles: Part2 重写！

--- part1/calc1.py
+++ part1/calc1.py
@@ -1,13 +1,13 @@
-
 EOF, INTEGER, PLUS = 'EOF', 'INTEGER', 'PLUS'
 
-class Token():
+class Token(object):
+
     def __init__(self, type, value):
         self.type = type
         self.value = value
 
     def __str__(self):
-        return 'Token({type}, {value})'.format(
+        return "Token({type},{value})".format(
             type=self.type,
             value=repr(self.value)
         )
@@ -15,10 +15,60 @@ class Token():
     def __repr__(self):
         return self.__str__()
 
-def main():
-    token = Token(INTEGER, 5)
-    print(token.__str__())
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
 
+    def error(self):
+        return Exception('Error parsing input')
+
+    def get_next_token(self):
+
+        if self.pos > len(self.text) - 1:
+            return Token(EOF,None)
+
+        current_char = self.text[self.pos]
+
+        if current_char.isdigit():
+            self.pos += 1
+            return Token(INTEGER,int(current_char))
+
+        if current_char == '+':
+            self.pos += 1
+            return Token(PLUS, '+')
+
+        self.error()
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            return self.error()
+
+    def expr(self):
+
+        self.current_token = self.get_next_token()
+
+        left = self.current_token
+        self.eat(INTEGER)
+
+        op = self.current_token
+        self.eat(PLUS)
+
+        right = self.current_token
+        self.eat(INTEGER)
+
+        return left.value + right.value
+
+def main():
+
+    while True:
+        text = input("calc1> ")
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
 
 if __name__ == "__main__":
     main()
---
+++ part2/calc2.py
@@ -0,0 +1,106 @@
+
+EOF, PLUS, MINUS,INTEGER = 'EOF', 'PLUS', 'MINUS','INTEGER'
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return "Token({type},{value})".format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        return Exception("Error parsing input")
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+
+        while self.current_char is not None:
+
+            # 先处理空格逻辑
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS,'+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            self.error()
+
+        return Token(EOF, None)
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token()
+
+        left = self.current_token
+        self.eat(INTEGER)
+
+        op = self.current_token
+        if op.type == PLUS:
+            self.eat(PLUS)
+        else:
+            self.eat(MINUS)
+
+        right = self.current_token
+        self.eat(INTEGER)
+
+        if op.type == PLUS:
+            result = left.value + right.value
+        elif op.type == MINUS:
+            result = left.value - right.value
+
+        return result
+
+def main():
+    while True:
+        text = input("calc2 > ")
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+if __name__ == "__main__":
+    main()
